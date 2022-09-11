b8ac7622f5593742effb1d75b897306b4faa4ceb
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-09-11 15:11:33 +0600

    cart layout is done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Categories from './components/Categories';
 import Header from './components/Header';
 import './App.css';
 import Item from './components/Item';
+import Cart from './components/Cart';
 
 export default class App extends Component {
   render = () => (
     <div className='container'>
       <Header/>
-      <Categories/>
-      <Item/>
+      {/* <Categories/>
+      <Item/> */}
+      <Cart/>
     </div>
   )
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,110 @@
 import React, { Component } from 'react';
+import classes from './Cart.module.css';
+import Item from '../assets/Product D.svg';
 
 export default class Cart extends Component {
   render = () => (
-    <div>
-      <div></div>
+    <div className={classes.cart}>
+      <div className={classes.cartTitle}>
+        <h1>CART</h1>
+      </div>
+      <div className={classes.line} />
+      <div>
+        <div className={classes.cartItem}>
+          <div>
+            <h1 className={classes.brandName}>
+              Apollo <span>Running Short</span>
+            </h1>
+            <div className={classes.price}>
+              <h2>$50.00</h2>
+            </div>
+            <div className={classes.size}>
+              <h2>SIZE:</h2>
+              <button>XS</button>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+            </div>
+            <div className={classes.color}>
+              <h2>COLOR:</h2>
+              <button />
+              <button />
+              <button />
+            </div>
+          </div>
+          <div>
+            <div className={classes.amountInfo}>
+              <div className={classes.amountBtn}>
+                <div>
+                  <button>+</button>
+                </div>
+                <div className={classes.amount}>1</div>
+                <div>
+                  <button>-</button>
+                </div>
+              </div>
+              <div className={classes.itemImg}>
+                <img src={Item} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={classes.line} />
+
+        <div className={classes.cartItem}>
+          <div>
+            <h1 className={classes.brandName}>
+              Apollo <span>Running Short</span>
+            </h1>
+            <div className={classes.price}>
+              <h2>$50.00</h2>
+            </div>
+            <div className={classes.size}>
+              <h2>SIZE:</h2>
+              <button>S</button>
+              <button>M</button>
+            </div>
+            <div className={classes.color}>
+              <h2>COLOR:</h2>
+              <button />
+              <button />
+              <button />
+            </div>
+          </div>
+          <div>
+            <div className={classes.amountInfo}>
+              <div className={classes.amountBtn}>
+                <div>
+                  <button>+</button>
+                </div>
+                <div className={classes.amount}>1</div>
+                <div>
+                  <button>-</button>
+                </div>
+              </div>
+              <div className={classes.itemImg}>
+                <img src={Item} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.line} />
+      <div>
+        <div>
+          <h1>Tax 21%</h1>
+          <h1>$42.00</h1>
+        </div>
+        <div>
+          <h1>Quantity</h1>
+          <h1>3</h1>
+        </div>
+        <div>
+          <h1>Total</h1>
+          <h1>$200.00</h1>
+        </div>
+      </div>
     </div>
   );
 }
